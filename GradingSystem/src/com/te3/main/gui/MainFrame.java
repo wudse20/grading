c3550386af920d81c4dc7adc62656943d7d862a0
wudse20 <antonskorup@gmail.com>
2020-02-11 13:14:30 +0100

    Update MainFrame.java

--- GradingSystem/src/com/te3/main/gui/MainFrame.java
+++ GradingSystem/src/com/te3/main/gui/MainFrame.java
@@ -1,5 +1,19 @@
 package com.te3.main.gui;
 
-public class MainFrame {
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame {
+	
+	/** Default */
+	private static final long serialVersionUID = 1L;
+
+	public MainFrame() 
+	{
+		super("Betygssättning");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(new Dimension(400, 300));
+	}
 
 }
