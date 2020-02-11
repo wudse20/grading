5c847b3b9d1c785bbfb0904176d270c6520560d9
wudse20 <antonskorup@gmail.com>
2020-02-11 20:39:50 +0100

    Update XML.java
    Made it take any type.

--- GradingSystem/src/com/te3/main/objects/XML.java
+++ GradingSystem/src/com/te3/main/objects/XML.java
@@ -8,17 +8,29 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 
-public class XML {
+/**
+ * XML de-/encoder
+ * 
+ * @author Anton Skorup
+ * @param <E> The data type of the object, that is to be saved.
+ */
+public class XML<E> {
 
 	public XML() {}
 
+	/**
+	 * Writes an XML-file of object
+	 * 
+	 * @param fileLocation The path to the file
+	 * @param obj          The object
+	 */
 	@SuppressWarnings("unused")
-	private void write(String fileLocation, ArrayList<Data> al) {
+	public void write(String fileLocation, E obj) {
 		try {
 			FileOutputStream fos = new FileOutputStream(new File(fileLocation));
 			XMLEncoder encoder = new XMLEncoder(fos);
 
-			encoder.writeObject(al);
+			encoder.writeObject(obj);
 			encoder.close();
 			fos.close();
 		} catch (IOException e) {
@@ -26,22 +38,27 @@ public class XML {
 		}
 	}
 
-	@SuppressWarnings("unchecked")
-	public static ArrayList<Data> read(String fileLocation) {
-		ArrayList<Data> al = new ArrayList<Data>();
-
+	/**
+	 * Reads the XML-file from an XML-File.
+	 * 
+	 * @param fileLocation the file path
+	 * @return the object from the XML File
+	 */
+	@SuppressWarnings({ "unchecked", "unused" })
+	public E read(String fileLocation) {
+		Object o = null;
 		try {
 			FileInputStream fis = new FileInputStream(new File(fileLocation));
 			XMLDecoder encoder = new XMLDecoder(fis);
 
-			al = (ArrayList<Data>) encoder.readObject();
+			o = (E) encoder.readObject();
 			fis.close();
 			encoder.close();
 
-			return al;
+			return (E) o;
 		} catch (IOException e) {
 			System.out.println(e.toString());
-			return al;
+			return (E) o;
 		}
 	}
 }
