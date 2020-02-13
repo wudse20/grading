261a76bedf91161b9620b55b84db8f4eeb5e33ac
wudse20 <antonskorup@gmail.com>
2020-02-13 11:00:20 +0100

    Update Criteria.java
    La till knappar och labels som skulle finnas här, för att det ska bli möjligt att göra gui:t basserat på antalet kriterier som är med.

--- GradingSystem/src/com/te3/main/objects/Criteria.java
+++ GradingSystem/src/com/te3/main/objects/Criteria.java
@@ -1,15 +1,31 @@
 package com.te3.main.objects;
 
+import javax.swing.JButton;
+import javax.swing.JLabel;
+
+import com.te3.main.exceptions.IllegalInputException;
+
 public class Criteria {
 	
 	private String name;
 	private char grade;
 	
+	private JButton[] gradeBtns = 
+			new JButton[] {new JButton("F"), new JButton("E"), new JButton("C"), new JButton("A")};
+	
+	private JLabel criteria;
+	
 	public Criteria() {}
 	
-	public Criteria(String name) {
-		setName(name);
-		setGrade('F');
+	public Criteria(String name) throws IllegalInputException {
+		this.setName(name);
+		try {
+			this.setGrade('F');
+		} catch (IllegalInputException e) {
+			throw new IllegalInputException(e.getMessage());
+		}
+		
+		criteria = new JLabel(name);
 	}
 	
 	public String getName() {
@@ -24,11 +40,11 @@ public class Criteria {
 		return this.grade;
 	}
 	
-	public void setGrade(char grade) {
+	public void setGrade(char grade) throws IllegalInputException {
 		if (grade == 'F' || grade == 'E' || grade == 'C' || grade == 'A') {
 			this.grade = grade;
 		} else {
-			//throw error? or set to default
+			throw new IllegalInputException("Inget betyg");
 		}
 	}
 
