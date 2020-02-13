3e87fc87fdbf5852f9ca64ce4ed12686fb8cbb81
wudse20 <antonskorup@gmail.com>
2020-02-13 21:41:07 +0100

    Create State.java
    Flyttade så att alla klasser har tillgång till enumet

---
+++ GradingSystem/src/com/te3/main/enums/State.java
@@ -0,0 +1,13 @@
+package com.te3.main.enums;
+
+/**
+ * An enum for the different states of this panel.
+ * 
+ * @author Anton Skorup
+ */
+public enum State {
+	/** A generalized view for all the criteria. */
+	SINGEL_STUDENT_GENERALIZED,
+	/** A view for all the assignments displaying the highest grade */
+	SINGEL_STUDENT_ASSIGNMENT;
+}
