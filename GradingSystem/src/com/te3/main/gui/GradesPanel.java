078e903702def698320411c5ac09889a910071bb
wudse20 <antonskorup@gmail.com>
2020-02-14 16:11:38 +0100

    Update GradesPanel.java
    Lagt till sidoinfon.

--- GradingSystem/src/com/te3/main/gui/GradesPanel.java
+++ GradingSystem/src/com/te3/main/gui/GradesPanel.java
@@ -1,11 +1,13 @@
 package com.te3.main.gui;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.util.ArrayList;
 
+import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -31,19 +33,20 @@ public class GradesPanel extends JPanel {
 
 	FlowLayout[] criteriaBtnLayouts;
 	GridLayout[] criteriaGridLayouts;
-	
+
 	JPanel panel = new JPanel();
 	JPanel panelInfo = new JPanel();
 	JPanel panelInfo2 = new JPanel();
 
 	BorderLayout layout = new BorderLayout();
 	BorderLayout pInfoLayout = new BorderLayout();
-	BoxLayout pInfoLayout2 = new BoxLayout(panelInfo2, BoxLayout.Y_AXIS); 
+	BoxLayout pInfoLayout2 = new BoxLayout(panelInfo2, BoxLayout.Y_AXIS);
 	BoxLayout pLayout;
 
 	JScrollPane scroll = new JScrollPane(panel);
-	
+
 	JLabel lblName = new JLabel();
+	JLabel lblAssingment = new JLabel();
 	JLabel lblGradedAsignments = new JLabel();
 	JLabel lblGrades = new JLabel();
 	JLabel lblSpacer1 = new JLabel(" ");
@@ -51,11 +54,11 @@ public class GradesPanel extends JPanel {
 	JLabel lblSpacer3 = new JLabel("    ");
 	JLabel lblSpacer4 = new JLabel(" ");
 	JLabel lblSpacer6 = new JLabel("    ");
-	
+
 	MainFrame mf;
 
 	public GradesPanel(MainFrame mf) {
-		this.state = State.SINGEL_STUDENT_GENERALIZED;
+		this.state = State.SINGEL_STUDENT_ASSIGNMENT;
 		this.mf = mf;
 		this.setLayout(layout);
 
@@ -64,13 +67,15 @@ public class GradesPanel extends JPanel {
 
 		updateGUI(state);
 		
+		scroll.setBorder(BorderFactory.createEmptyBorder());
+
 		this.add(lblSpacer1, BorderLayout.PAGE_START);
 		this.add(panelInfo, BorderLayout.LINE_START);
 		this.add(scroll, BorderLayout.CENTER);
 		this.add(lblSpacer6, BorderLayout.LINE_END);
 	}
 
-	private void updateGUI(State s) {
+	public void updateGUI(State s) {
 		if (s.equals(State.SINGEL_STUDENT_ASSIGNMENT)) {
 			dispalyCriteria(criteria);
 		} else {
@@ -80,7 +85,7 @@ public class GradesPanel extends JPanel {
 				for (var j = 0; j < tasks.get(i).getCriteria().size(); j++) {
 					Criteria c = tasks.get(i).getCriteria().get(j);
 					int index = displayedGrades.indexOf(c);
-					
+
 					if (index != -1) {
 						if (!(displayedGrades.get(index).compare(c))) {
 							displayedGrades.remove(index);
@@ -92,44 +97,71 @@ public class GradesPanel extends JPanel {
 					}
 				}
 			}
-			
+
 			dispalyCriteria(displayedGrades);
 		}
-		
-		updateInfo(tasks.get(mf.getCurrentlySelectedAssingmentIndex()).getStudents().get(mf.getCurrentlySelectedStudentIndex()));
-		
+
 		// Updates the frame with the new components
 		mf.revalidate();
 		mf.repaint();
 	}
 
-	private void updateInfo(Student s) {
+	private void updateInfo(Student s, Task t, ArrayList<Criteria> al) {
 		lblName.setText(s.getName());
 		lblGradedAsignments.setText("(" + s.getCompletedTasks() + "/" + s.getNumOfTasks() + ")");
-		lblGrades.setText(countGrades(s));
-		
+		lblGrades.setText(countGrades(al));
+		lblAssingment.setText(t.getName());
+
 		lblName.setFont(new Font(lblName.getFont().getName(), Font.BOLD, 20));
 		lblGradedAsignments.setFont(new Font(lblName.getFont().getName(), Font.PLAIN, 20));
 		lblGrades.setFont(new Font(lblName.getFont().getName(), Font.PLAIN, 20));
-		
+		lblAssingment.setFont(new Font(lblAssingment.getFont().getName(), Font.PLAIN, 20));
+
 		panelInfo.setLayout(pInfoLayout);
-		panelInfo2.setLayout(pInfoLayout2);;
-		
+		panelInfo2.setLayout(pInfoLayout2);
+
 		panelInfo2.add(lblName);
+		panelInfo2.add(lblAssingment);
 		panelInfo2.add(lblGradedAsignments);
 		panelInfo2.add(lblGrades);
-		
+
 		panelInfo.add(lblSpacer2, BorderLayout.LINE_START);
 		panelInfo.add(panelInfo2, BorderLayout.CENTER);
 		panelInfo.add(lblSpacer3, BorderLayout.LINE_END);
 		panelInfo.add(lblSpacer4, BorderLayout.PAGE_END);
 	}
 
-	private String countGrades(Student s) {
-		return "Not yet implemented";
+	private String countGrades(ArrayList<Criteria> al) {
+		int f = 0, e = 0, c = 0, a = 0;
+		
+		for (int i = 0; i < al.size(); i++) {
+			switch (al.get(i).getGrade()) {
+				case F:
+					f++;
+					break;
+				case E:
+					e++;
+					break;
+				case C:
+					c++;
+					break;
+				case A:
+					a++;
+					break;
+			}
+		}
+		
+		return "F: " + f + ", E: " + e + ", C: " + c + ", A: " + a;
 	}
 
 	private void dispalyCriteria(ArrayList<Criteria> criteria) {
+		//Removes all components
+		for (Component c : panel.getComponents())
+			panel.remove(c);
+		
+		panel.revalidate();
+		panel.repaint();
+		
 		criteriaBtnPanels = new JPanel[criteria.size()];
 		criteriaPanelsNeedsNewName = new JPanel[criteria.size()];
 		criteriaBtnLayouts = new FlowLayout[criteria.size()];
@@ -144,24 +176,28 @@ public class GradesPanel extends JPanel {
 			criteriaBtnLayouts[i] = new FlowLayout(FlowLayout.LEFT);
 			criteriaGridLayouts[i] = new GridLayout(2, 2);
 		}
-		
+
 		for (int i = 0; i < criteria.size(); i++) {
 			Criteria c = criteria.get(i);
 			JButton[] b = c.getGradeBtns();
 			criteriaBtnPanels[i].setLayout(criteriaBtnLayouts[i]);
 			criteriaPanelsNeedsNewName[i].setLayout(criteriaGridLayouts[i]);
-			
+
 			for (int j = 0; j < b.length; j++) {
 				criteriaBtnPanels[i].add(b[j]);
 			}
-			
-			//criteriaPanels[i].add(c.getLblCriteria(), BorderLayout.LINE_START);
+
 			criteriaPanelsNeedsNewName[i].add(c.getLblCriteria());
 			criteriaPanelsNeedsNewName[i].add(c.getLblGrade());
 			criteriaPanelsNeedsNewName[i].add(criteriaBtnPanels[i]);
 
 			panel.add(criteriaPanelsNeedsNewName[i]);
 		}
+		
+		updateInfo(
+				tasks.get(mf.getCurrentlySelectedAssingmentIndex()).getStudents()
+						.get(mf.getCurrentlySelectedStudentIndex()),
+				tasks.get(mf.getCurrentlySelectedAssingmentIndex()), criteria);
 	}
 
 	public State getState() {
