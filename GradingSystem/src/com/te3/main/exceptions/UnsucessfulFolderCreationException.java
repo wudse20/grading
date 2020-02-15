4530d9f296acdbd567a210f249ae5e438b6db91a
wudse20 <antonskorup@gmail.com>
2020-02-15 21:51:10 +0100

    Create UnsucessfulFolderCreationException.java
    För filsystemet

---
+++ GradingSystem/src/com/te3/main/exceptions/UnsucessfulFolderCreationException.java
@@ -0,0 +1,22 @@
+package com.te3.main.exceptions;
+
+public class UnsucessfulFolderCreationException extends Exception {
+
+	/** Default */
+	private static final long serialVersionUID = 1L;
+	
+	private String message;
+	
+	public UnsucessfulFolderCreationException() {
+		this.message = "Något gick fel";
+	}
+	
+	public UnsucessfulFolderCreationException(String message) {
+		this.message = (message.trim().equals("")) ? "Något gick fel" : message;
+	}
+	
+	@Override
+	public String getMessage() {
+		return this.message;
+	}
+}
